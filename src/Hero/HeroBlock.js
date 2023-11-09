afdba1e154f15f70321459667327d0ee02de2a24
secret631 <aseman00631@gmail.com>
2023-11-09 12:01:51 +0330

    search center

--- src/Hero/HeroBlock.js
+++ src/Hero/HeroBlock.js
@@ -10,53 +10,53 @@ import SearchIcon from '@mui/icons-material/Search';
 import DirectionsIcon from '@mui/icons-material/Directions';
 
 const HeroBlock = (props) => {
-    const [keyword, setkeyword] = useState('')
+  const [keyword, setkeyword] = useState('')
 
-    const SearchSubmit = (e) => {
-        setkeyword(e.target.value)
-    }
+  const SearchSubmit = (e) => {
+    setkeyword(e.target.value)
+  }
 
-    const SearchInputRendeler = () => {
-        return(
-            <div>
-                 <Paper
-      component="form"
-      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
-    >
-      <IconButton sx={{ p: '10px' }} aria-label="menu">
-        <MenuIcon />
-      </IconButton>
-      <InputBase
-
-      onChange={
-        SearchSubmit
-      }
-        sx={{ ml: 1, flex: 1 }}
-        placeholder="search your product"
-        inputProps={{ 'aria-label': 'search your product' }}
-      />
-      <IconButton  type="button" sx={{ p: '10px' }} aria-label="search">
-        <SearchIcon onClick={() => props.submitHandler(keyword)}/>
-      </IconButton>
-      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-      <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions">
-        <DirectionsIcon />
-      </IconButton>
-    </Paper>
-            </div>
-        )
-    }
-
+  const SearchInputRendeler = () => {
     return (
-        <div className="Hero">
-            <img src={hero} />
-            {/* <p>Search your items</p> */}
-            {/* <input onChange={SearchSubmit} /> */}
-            {SearchInputRendeler()}
-            {/* <button onClick={() => props.submitHandler(keyword)}>Search</button> */}
+      <div className="Form_input">
+        <Paper
+          component="form"
+          sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+        >
+          <IconButton sx={{ p: '10px' }} aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <InputBase
 
-
-        </div>
+            onChange={
+              SearchSubmit
+            }
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="search your product"
+            inputProps={{ 'aria-label': 'search your product' }}
+          />
+          <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+            <SearchIcon onClick={() => props.submitHandler(keyword)} />
+          </IconButton>
+          <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+          <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions">
+            <DirectionsIcon />
+          </IconButton>
+        </Paper>
+      </div>
     )
+  }
+
+  return (
+    <div className="Hero">
+      <img src={hero} />
+      {/* <p>Search your items</p> */}
+      {/* <input onChange={SearchSubmit} /> */}
+      {SearchInputRendeler()}
+      {/* <button onClick={() => props.submitHandler(keyword)}>Search</button> */}
+
+
+    </div>
+  )
 }
 export default HeroBlock
