ddc7c75195e970e7f7fcf563791c6998f64e9116
secret631 <aseman00631@gmail.com>
2023-11-17 17:43:05 +0330

    photo?

--- src/CardList/Card.js
+++ src/CardList/Card.js
@@ -17,7 +17,7 @@ const Card = (props) => {
 
     return (
         <div >
-            <Link to={{ pathname: '/ProProductDetails' }}>
+            <Link to={{ pathname: '/ProProductDetails' }} state={props.OBG.id}>
 
                 <img src={src}
                     style={{
--- src/ProductPage/ProductPage.js
+++ src/ProductPage/ProductPage.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './ProductPage.css'
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 const ProductPage= ()=>{
-    console.log('pppppppp')
+const [photo,setphoto] = useState(null)
+const location= useLocation()
+
+useEffect(() => {
+    axios.get(`https://api.unsplash.com/photos${location.state}`, {
+        headers: {
+            Authorization: 'Client-ID 8GMrSwACBn1O6TtOVIrzpWDNqkeLaEGMyW5-A5Y6eZ0'
+        }
+    }).then(Response => {
+
+        console.log(Response)
+        setphoto(Response.data)
+    })
+
+}, [])
     return(
         <div >
-            ppppppp
+            <img src={photo?.urls.regular} />
+            
         </div>
     )
 }
