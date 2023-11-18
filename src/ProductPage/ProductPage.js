5f0afc827a45e2be1dd9d91b60ec9fa3f45c0dbb
secret631 <aseman00631@gmail.com>
2023-11-18 14:26:06 +0330

    ProductImage

--- src/ProductPage/ProductPage.js
+++ src/ProductPage/ProductPage.js
@@ -1,45 +1,44 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import './ProductPage.css'
 import { useLocation, useParams } from "react-router-dom";
 import axios from "axios";
 
-const ProductPage= ()=>{
-const params = useParams()
+const ProductPage = () => {
+    const params = useParams()
 
-const [photo,setphoto] = useState(null)
-console.log(params)
+    const [photo, setphoto] = useState(null)
+    console.log(params)
 
-useEffect(() => {
-    axios.get(`https://api.unsplash.com/photos/${params.Id}`, {
-        headers: {
-            Authorization: 'Client-ID 8GMrSwACBn1O6TtOVIrzpWDNqkeLaEGMyW5-A5Y6eZ0'
-        }
-    }).then(Response => {
-        console.log(Response)
-        setphoto(Response.data)
-    })
+    useEffect(() => {
+        axios.get(`https://api.unsplash.com/photos/${params.Id}`, {
+            headers: {
+                Authorization: 'Client-ID 8GMrSwACBn1O6TtOVIrzpWDNqkeLaEGMyW5-A5Y6eZ0'
+            }
+        }).then(Response => {
+            console.log(Response)
+            setphoto(Response.data)
+        })
 
-}, [])
+    }, [])
+
+    return (
+        <div className="ProductPage">
+            <div className="Profile">
+                <img src={photo?.user.profile_image.medium} />
+                <p>{photo?.user.name}</p>
+            </div>
+
+
+            <div className="ProductImage">
+                <img src={photo?.urls.regular} />
+            </div>
 
 
 
-// useEffect(() => {
-//     axios.get(`https://api.unsplash.com/photos${location.state}`, {
-//         headers: {
-//             Authorization: 'Client-ID 8GMrSwACBn1O6TtOVIrzpWDNqkeLaEGMyW5-A5Y6eZ0'
-//         }
-//     }).then(Response => {
 
-//         console.log(Response)
-//         setphoto(Response.data)
-//     })
-
-// }, [])
-    return(
-        <div >
-            <img src={photo?.urls.regular} />
             
         </div>
+
     )
 }
 export default ProductPage
